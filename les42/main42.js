cf002fad4da747e96760d14f3d9b47c140023472
unknown <Vinh.pn260701@gmail.com>
2021-11-30 22:44:06 +0700

    add 4

---
+++ les42/main42.js
@@ -0,0 +1,23 @@
+
+//if else
+
+var date = 2;
+
+if(date === 2){
+    console.log('今日は月曜日です。')
+}　else if(date > 0){
+    console.log('今日は火曜日です。')
+}　else if(date === 4){
+    console.log('今日は水曜日です。')
+}　else if(date === 5){
+    console.log('今日は木曜日です。')
+}　else if(date === 6){
+    console.log('今日は金曜日です。')
+}　else if(date === 7){
+    console.log('今日は土曜日です。')
+}　else if(date === 8){
+    console.log('今日は日曜日です。')
+}
+
+// chỉ thực thi duy nhất 1 nhánh
+// không thực hiện đồng thời nhiều hơn 1 nhánh
---
+++ les43/main43.js
@@ -0,0 +1,35 @@
+// switch case
+
+var date = 9
+
+switch(date)　{
+    case 2: {　// date === 2
+        console.log('今日は月曜日です。')
+        break
+    }
+    case 3: {
+        console.log('今日は火曜日です。')
+        break
+    }
+    case 4: {
+        console.log('今日は水曜日です。')
+        break
+    }
+    case 5: {
+        console.log('今日は木曜日です。')
+        break
+    }
+    case 6: {
+        console.log('今日は金曜日です。')
+        break
+    }
+    case 7: {
+        console.log('今日は土曜日です。')
+        break
+    }
+    case 8: {
+        console.log('今日は日曜日です。')
+        break
+    }
+    default: console.log('今日は何曜日ですか。')
+}
